9faf6b029880976549fc162f7459e1741a95f903
Millie <millie@mltn.xyz>
2022-06-23 18:55:35 -0600

    Beautiful first commit
    Yay...

---
+++ Module1/CH1HelloWorld/TestHello/TestHello/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CH 1 Hello World by James Sales");
+            Console.WriteLine("\nHello, Earthlings! We are here to start the Zombie Apocalypse");
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
