54a0e889ec636c6bd68b2198dd3c7856fae92dea
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-06-27 18:34:40 -0600

    Finished assignment
    This required adding an output text field and adding the code for the okClick button on line 33 of MainPage.xaml.cs to change the text of the newly added text box to be as the assignment required.

--- Module1/CH1HelloWorld/GUIHelloWorld/GUIHelloWorld/MainPage.xaml.cs
+++ Module1/CH1HelloWorld/GUIHelloWorld/GUIHelloWorld/MainPage.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Popups;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -27,9 +28,12 @@ namespace GUIHelloWorld
             this.InitializeComponent();
         }
 
+        //When the OK button is clicked it runs the following.
+        //This only sets the ouput field's text to the greeting with the text value of the userName field.
         private void okClick(object sender, RoutedEventArgs e)
         {
-
+            output.Text = "Greetings, " + userName.Text + ", pleased to meet you!";
+            
         }
     }
 }
