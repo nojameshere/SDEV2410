856f6799d75e66eecfa5a62fbed70820a075b0e6
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-06-28 19:35:37 -0600

    Finished CH2
    Console apps are SO MUCH BETTER than GUI apps. I hate GUI apps with a passion. Maybe Universal forms apps are better than Windows Forms. But I still don't like it. It's boring and gross.

---
+++ Module1/CH2WorkingWithVariablesOperatorsAndExpressions/ProjSnackStand/CH2MathOperations/CH2MathOperations/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("CH2 Math Operations by James Sales");
+//Gets entered numbers from user and immediately assigns it to a variable
+
+int num1 = 0;
+int num2 = 0;
+int num3 = 0;
+num1 = num2 = num3 = 20;
+Console.WriteLine(num1);
+Console.WriteLine(num2);
+Console.WriteLine(num3);
+
+
+//the program needs to pause here
+Console.WriteLine("\nPress any key to continue...");
+Console.ReadKey();
+
