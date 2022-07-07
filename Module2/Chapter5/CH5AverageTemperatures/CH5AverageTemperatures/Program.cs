835e6197824f9a8a90d8fefc6f5f8466223acf67
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-07-07 15:03:01 -0600

    Small fix for nested loops
    Instructor isn't making much sense and I need to get the program reviewed first.

--- Module2/Chapter5/CH5AverageTemperatures/CH5AverageTemperatures/Program.cs
+++ Module2/Chapter5/CH5AverageTemperatures/CH5AverageTemperatures/Program.cs
@@ -4,37 +4,35 @@ double low = 0;
 double high = 0;
 double average = 0;
 string continueRunning = "y";
+bool dataCheck = true;
 
 do
 {
     low = 0;  //Need to set these for the while structure for data validation.
     high = 0;
-    while (low == 0)
+    while (dataCheck)
     {
         Console.Write("Enter low temperature: ");
         low = double.Parse(Console.ReadLine());
-        if (low > -51 && low < 121) //Validation for the low temp
+        if (low > -51)
         {
-            while (high == 0)
-            {
-                Console.Write("Enter high temperature: ");
-                high = double.Parse(Console.ReadLine());
-                if (high > low && high < 121) //Validation for the high temp
-                {
-                    average = (low + high) / 2;
-                    Console.WriteLine($"Average for {low} and {high} is {average}");
-                    Console.Write("Do you have more temperatures? (y/n) ");
-                    continueRunning = Console.ReadLine();
-                }
-                else { high = 0; }  //To keep the while functioning if the validation doesn't pass
-            }
-
+            dataCheck = false;
         }
-        else { low = 0; } //To keep the while functioning if the validation doesn't pass
     }
-
-
-
+    dataCheck = true;
+    while (dataCheck)
+    {
+        Console.Write("Enter high temperature: ");
+        high = double.Parse(Console.ReadLine());
+        if (high > low && high < 121)
+        {
+            dataCheck = false;
+        }
+    }
+    average = (low + high) / 2;
+    Console.WriteLine($"Average for {low} and {high} is {average}");
+    Console.Write("Do you have more temperatures? (y/n) ");
+    continueRunning = Console.ReadLine();
 }
 while (continueRunning != "n"); //Will cause the do to continue running until a user types n
 
