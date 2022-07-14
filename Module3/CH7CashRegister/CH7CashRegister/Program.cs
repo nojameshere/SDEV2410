aa138263e5eeea276034f69748b2d746d9eb0b89
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-07-13 20:30:29 -0600

    yes

---
+++ Module3/CH7CashRegister/CH7CashRegister/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Chapter 7 Cash Register by James Sales");
+bool continueRunning = true;
+
+class CashRegister
+{
+    private double total = 0; //The only variable for this class
+    public void CashRegister() //This is the constructor
+    {
+        //Stuff to do when constructed
+    }
+
+    public double NewSale()
+    {
+        //ask for cost of item
+        //ask for money from customer
+        //check if enough money given
+        //calculate change
+        //update total
+    }
+
+    public double TotalSales()
+    {
+
+    }
+}
+while (continueRunning = true)
+{
+    //do the thing.
+}
