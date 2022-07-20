c79b4c76e2196b449d056798b1185ce52e5cf0b4
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-07-20 17:58:08 -0600

    Finished the first 2 assignments of chapter 7
    Its been a tough few days this week but I am carrying on.

--- Module3/CH7CashRegister/CH7CashRegister/Program.cs
+++ Module3/CH7CashRegister/CH7CashRegister/Program.cs
@@ -1,29 +1,53 @@
 ﻿Console.WriteLine("Chapter 7 Cash Register by James Sales");
 bool continueRunning = true;
-
+CashRegister register = new CashRegister();
+while (continueRunning)
+{
+    //do the thing.
+    register.NewSale();
+    register.Change();
+    Console.Write("More purchases? (y/n) ");
+    if (Console.ReadLine() == "n")
+    {
+        continueRunning = false;
+    }
+}
+Console.WriteLine($"\nTotal Sales: ${register.total:N2}");
+Console.WriteLine("\nPress any key to continue");
+Console.ReadKey();
 class CashRegister
 {
-    private double total = 0; //The only variable for this class
-    public void CashRegister() //This is the constructor
+    public double total = 0; //The only variable for this class
+    double cost = 0;
+    double tender = 0;
+    double change = 0;
+    public CashRegister() //This is the constructor
     {
         //Stuff to do when constructed
     }
 
-    public double NewSale()
+    public void NewSale()
     {
+        Console.Write("\nWhat is the cost? $");
+        cost = double.Parse(Console.ReadLine());
+        Console.Write("How much money from customer? $");
+        tender = double.Parse(Console.ReadLine());
         //ask for cost of item
         //ask for money from customer
-        //check if enough money given
-        //calculate change
-        //update total
     }
 
-    public double TotalSales()
+    public void Change()
     {
-
+        change = tender - cost;
+        if (change < 0)
+        {
+            Console.WriteLine("No sale, you didn't pay enough");
+        }
+        else
+        {
+            Console.WriteLine($"The change is ${change:N2}");
+            total = total + cost;
+        }
+        //calculate change and catch error for not enough money
     }
-}
-while (continueRunning = true)
-{
-    //do the thing.
 }
---
+++ Module3/CH7Inventory/CH7Inventory/Program.cs
@@ -0,0 +1,84 @@
+﻿Console.WriteLine("Chapter 7 Inventory by James Sales");
+
+string name = "";
+int ID = 0;
+int quantity = 0;
+double cost = 0;
+//Class for inventory items
+//in a FOR loop
+//create 3 objects
+//user inputs data for each object
+for (int i = 0; i < 3; i++)
+{
+    try
+    {
+        Console.Write("\nEnter the ID of the item: ");
+        ID = int.Parse(Console.ReadLine());
+    }
+    catch(FormatException)
+    {
+        ID = 1000;
+        Console.WriteLine("Error in item ID, using 1000");
+    }
+    Console.Write("Enter the name of item: ");
+    name = Console.ReadLine();
+    try
+    {
+        Console.Write("Enter quantity in stock: ");
+        quantity = int.Parse(Console.ReadLine());
+    }
+    catch(FormatException)
+    {
+        quantity = 1;
+        Console.WriteLine("Error in item quantity, using 1");
+    }
+    try
+    {
+        Console.Write("Enter price of item: ");
+        cost = double.Parse(Console.ReadLine());
+    }
+    catch
+    {
+        cost = 0.01;
+        Console.WriteLine("Error in item cost, using 0.01");
+    }
+    Inventory item;
+    item = new Inventory(name, ID, quantity, cost);
+    item.displayItem();
+}
+//use try/catch for user input
+//assign default values when data is invalid
+
+//Have shared variable for total value of all items
+//Display value at end of run
+Console.WriteLine($"\nTotal value of current inventory: ${Inventory.total:N2}");
+
+Console.WriteLine("\nPress the any key to continue.   .   .");
+Console.ReadKey();
+
+class Inventory
+{
+    string name = "";
+    int ID = 0;
+    int quantity = 0;
+    double cost = 0;
+    public static double total;
+
+    public Inventory(string name, int ID, int quantity, double cost)
+    {
+        this.name = name;
+        this.ID = ID;
+        this.quantity = quantity;
+        this.cost = cost;
+        total = total + (cost * quantity);
+
+        //Use expression bodied method to list ID and Name
+    }
+
+    public string printName() => $"#{ID} {name}:";
+
+    public void displayItem()
+    {
+        Console.WriteLine($"\n{printName()} {quantity} at ${cost} each, value ${quantity * cost:N2}");
+    }
+}
