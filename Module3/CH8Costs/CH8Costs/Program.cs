68fcbb02a930ad13816e7e6ec5a95f423fb3b73c
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-07-27 20:24:44 -0600

    Chapter 8 almost done
    I'm so sick and tired of these damn reference variables. I'm still learning how they work in classes and I'm too scatterbrained to figure it out tonight. This is a tomorrow me problem (they said foreshadowing future failures)

---
+++ Module3/CH8Costs/CH8Costs/Program.cs
@@ -0,0 +1,82 @@
+﻿Console.WriteLine("Chapter 8 Costs by James Sales");
+double subtotal = 0;
+bool cont = true;
+
+Hardware mouse = new Hardware("RAZER GAMING MOUSE FOR GAMERS", 1146, 129.99);
+Hardware keyboard = new Hardware("Generic free with computer USB keyboard (used)", 492, 0.35);
+Repair display = new Repair("Display repair", 22, 49.95);
+Repair charge = new Repair("Charging port repair", 19, 29.99);
+
+
+
+while (cont)
+{
+//Some shit here about doing shit
+
+
+}
+
+
+
+
+
+class Hardware
+{
+    string name;
+    int ID;
+    double price = 0;
+    double tax = 0.075;
+
+    public Hardware(string hardwareName, int hardwareID, double hardwarePrice)
+    {
+        this.name = hardwareName;
+        this.ID = hardwareID;
+        this.price = hardwarePrice;
+    }
+    public double Calculation(ref subtotal)
+    {
+        Console.Write("How many sold? ");
+        int quantity = int.Parse(Console.ReadLine());
+        subtotal = quantity * this.price;
+        Console.WriteLine($"Subtotal = {subtotal}";
+        Console.WriteLine($"Tax = {subtotal * this.tax}");
+        double totalCost = subtotal + (subtotal * this.tax);
+        Console.WriteLine($"Total Cost = {(subtotal) + (subtotal * this.tax)}");
+        return totalCost
+    }
+    public string Display()
+    {
+        return $"{this.ID} {this.name}";
+    }
+}
+
+class Repair
+{
+    string name;
+    int ID;
+    double price = 0;
+    static double tax = 0.045;
+    static double markup = 0.1;
+
+    public Repair(string repairName, int repairID, double repairPrice)
+    {
+        this.name = repairName;
+        this.ID = repairID;
+        this.price = repairPrice;
+    }
+    public double Calculation(ref subtotal)
+    {
+        Console.Write("How many sold? ");
+        int quantity = int.Parse(Console.ReadLine());
+        subtotal = quantity * this.price;
+        Console.WriteLine($"Subtotal = {subtotal}";
+        Console.WriteLine($"Taxes and Fees = {(subtotal * this.tax) + (subtotal * this.markup)}");
+        double totalCost = subtotal + (subtotal * this.tax) + (subtotal * this.markup);
+        Console.WriteLine($"Total Cost = {(subtotal) + (subtotal * this.tax)}");
+        return totalCost
+    }
+    public string Display()
+    {
+        return $"{this.ID} {this.name}";
+    }
+}
--- Module3/CH8ReferenceValueParameters/CH8ReferenceValueParameters/Program.cs
+++ Module3/CH8ReferenceValueParameters/CH8ReferenceValueParameters/Program.cs
@@ -1,6 +1,43 @@
 ﻿Console.WriteLine("Chapter 8 Reference and Value Parameters by James Sales\n");
 
-void SquareRef(ref int refNum)
+double thisTemp = 211;
+
+Console.WriteLine($"\nOriginal value of thisTemp = {thisTemp}");
+Console.WriteLine($"Original value of outTemp is unassigned");
+
+Console.WriteLine($"\nCalling valTemps with valuable variable, this temp = {thisTemp}");
+valTemps(thisTemp);
+Console.WriteLine($"Return from valTemps, thisTemp = {thisTemp}");
+
+Console.WriteLine($"\nCalling refTemps with ref variable, thisTemp = {thisTemp}");
+refTemps(ref thisTemp);
+Console.WriteLine($"Return from refTemps, thisTemp = {thisTemp}");
+
+Console.WriteLine("\nCalling outTemps with unassigned variable");
+outTemps(out double outTemp);
+Console.WriteLine($"Return from outTemps, outTemp = {outTemp}");
+
+void refTemps(ref double refTemp)
 {
-    Console.WriteLine(
+    Console.WriteLine($"entering refTemp, ref variable = {refTemp}");
+    refTemp = (refTemp - 32) * (5.0 / 9.0);
+    Console.WriteLine($"leaving refTemp, ref variable = {refTemp}");
 }
+
+void outTemps(out double outTemp)
+{
+    Console.WriteLine("Entering outTemps with variable unassigned");
+    outTemp = 98.6;
+    outTemp = (outTemp - 32) * (5.0 / 9.0);
+    Console.WriteLine($"leaving outTemps, out variable = {outTemp}");
+}
+
+void valTemps(double thisTemp)
+{
+    Console.WriteLine($"entering valtemps, value variable = {thisTemp}");
+    thisTemp = (thisTemp - 32) * (5.0 / 9.0);
+    Console.WriteLine($"leaving valtemps, value variable = {thisTemp}");
+}
+
+Console.WriteLine("\nPress any key to proceed. . .");
+Console.ReadKey();
