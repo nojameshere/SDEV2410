8421a97e1a6b6d3e936de85af5ef65bd1f7d2a58
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-07-28 20:11:25 -0600

    Boxing objects is neat
    I fucking hate the wording of that assignment though.

--- Module3/CH8Costs/CH8Costs/Program.cs
+++ Module3/CH8Costs/CH8Costs/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Chapter 8 Costs by James Sales");
+﻿Console.WriteLine("Chapter 8 Costs by James Sales\n");
 double subtotal = 0;
 bool cont = true;
 int enteredID;
@@ -8,22 +8,61 @@ Hardware keyboard = new Hardware("Generic free with computer USB keyboard (used)
 Repair display = new Repair("Display repair", 22, 49.95);
 Repair charge = new Repair("Charging port repair", 19, 29.99);
 
-
+Console.WriteLine(mouse.Display());
+Console.WriteLine(keyboard.Display());
+Console.WriteLine(display.Display());
+Console.WriteLine(charge.Display());
 
 while (cont)
 {
-    //Some shit here about doing shit
-    Console.Write("Which one is being processed? ");
+    Console.Write("\nWhich one is being processed? ");
     enteredID = int.Parse(Console.ReadLine());
-    
+    Object obj;
+    if (enteredID == mouse.ID)
+    {
+        obj = mouse;
+    }
+    else if (enteredID == keyboard.ID)
+    {
+        obj = keyboard;
+    }
+    else if (enteredID == display.ID)
+    {
+        obj = display;
+    }
+    else if (enteredID == charge.ID)
+    {
+        obj = charge;
+    }
+    else 
+    {
+        continue;
+    }
+    if (obj is Hardware)
+    {
+        Hardware hardware = (Hardware)obj;
+        hardware.Calculation(ref subtotal);
+    }
+    else if (obj is Repair)
+    {
+        Repair repair = (Repair)obj;
+        repair.Calculation(ref subtotal);
+    }
+    Console.Write("Do you want to process another one? (y/n) ");
+    if (Console.ReadLine() == "n")
+    {
+        cont = false;
+    }
 }
 
+Console.WriteLine("Press a key to continue");
+Console.ReadKey();
 class Hardware
 {
     string name;
-    int ID;
+    public int ID;
     double price = 0;
-    double tax = 0.075;
+    static double tax = 0.075;
 
     public Hardware(string hardwareName, int hardwareID, double hardwarePrice)
     {
@@ -36,11 +75,11 @@ class Hardware
         Console.Write("How many sold? ");
         int quantity = int.Parse(Console.ReadLine());
         subtotal = quantity * this.price;
-        Console.WriteLine($"Subtotal = {subtotal}");
-        Console.WriteLine($"Tax = {subtotal * this.tax}");
-        double totalCost = subtotal + (subtotal * this.tax);
-        Console.WriteLine($"Total Cost = {(subtotal) + (subtotal * this.tax)}");
-        return totalCost;
+        Console.WriteLine($"Subtotal = ${subtotal:N2}");
+        Console.WriteLine($"Tax = ${subtotal * tax:N2}");
+        double additionalCost = (subtotal * tax);
+        Console.WriteLine($"Total Cost = ${(subtotal) + (subtotal * tax):N2}");
+        return additionalCost;
     }
     public string Display()
     {
@@ -51,7 +90,7 @@ class Hardware
 class Repair
 {
     string name;
-    int ID;
+    public int ID;
     double price = 0;
     static double tax = 0.045;
     static double markup = 0.1;
@@ -64,14 +103,12 @@ class Repair
     }
     public double Calculation(ref double subtotal)
     {
-        Console.Write("How many sold? ");
-        int quantity = int.Parse(Console.ReadLine());
-        subtotal = quantity * this.price;
-        Console.WriteLine($"Subtotal = {subtotal}");
-        Console.WriteLine($"Taxes and Fees = {(subtotal * tax) + (subtotal * markup)}");
-        double totalCost = subtotal + (subtotal * tax) + (subtotal * markup);
-        Console.WriteLine($"Total Cost = {(subtotal) + (subtotal * tax)}");
-        return totalCost
+        subtotal = this.price;
+        Console.WriteLine($"Subtotal = ${subtotal:N2}");
+        Console.WriteLine($"Taxes and Fees = ${(subtotal * tax) + (subtotal * markup):N2}");
+        double additionalCost = (subtotal * tax) + (subtotal * markup);
+        Console.WriteLine($"Total Cost = ${(subtotal) + (subtotal * tax):n2}");
+        return additionalCost;
     }
     public string Display()
     {
