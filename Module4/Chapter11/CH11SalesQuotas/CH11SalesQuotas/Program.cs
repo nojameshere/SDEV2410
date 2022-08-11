8795fc7ee82dc72f1dfc6e99d2b31f5d5fcecb24
James Sales <39015355+nojameshere@users.noreply.github.com>
2022-08-10 19:42:21 -0600

    Fuck this

---
+++ Module4/Chapter11/CH11SalesQuotas/CH11SalesQuotas/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Chapter 11 Sales Quotas by James Sales");
+
+//I don't fucking know anymore. I can't think or focus on this tonight. I had motivation but that was diminished by the grades I recieved on two previous products.
+
+
+
+struct Employee
+{
+    public string first;
+    string last;
+    public double quota;
+    public Employee(string fname, string lname, double quota)
+    {
+        this.first = fname;
+        this.last = lname;
+        this.quota = quota;
+    }
+    public string Display() { return $"{first} {last}"; }
+}
